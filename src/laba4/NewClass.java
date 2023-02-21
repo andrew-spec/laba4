cbab457d453e10938a120f94856b74fcba0fa069
20403 <20403@1k129s10.dune.edu>
2023-02-21 15:10:57 +0200

    создали класс

---
+++ src/laba4/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laba4;
+
+/**
+ *
+ * @author 20403
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(" world!");
+        
+        
+        
+        
+    }
+    
+}
