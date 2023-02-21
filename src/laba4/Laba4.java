0cb961ceea1711554bedb299c429e548082fbcef
20403 <20403@1k129s10.dune.edu>
2023-02-21 14:49:00 +0200

    поздоровались с миром

--- src/laba4/Laba4.java
+++ src/laba4/Laba4.java
@@ -16,7 +16,9 @@ public class Laba4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hsodi");
+        System.out.println("Helo world!");
+        
+        System.out.println("Helo!");
         
         
     }
